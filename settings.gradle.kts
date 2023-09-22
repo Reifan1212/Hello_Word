0b7f2546913477d44864af2b75e24403cf2dc781
Reifan1212 <riogit644@gmail.com>
2023-09-22 15:13:54 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Rio Reifan_F55122006_Pemrograman  Mobile"
+include(":app")
+ 
